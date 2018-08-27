47fda5d5155c328280fc05d5ee2d565167756ca9
alexloaiza1290 <alexloaiza.12.90@gmail.com>
2018-08-27 09:46:22 -0500

    Creacion del constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "FusionAis.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    FusionAis *fusion = new FusionAis();
+    fusion->eliminarFusion();
     return 0;
 }
